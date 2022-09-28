b0a52bccb1c85b1c216a73a881b98d58532e0ec2
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-28 19:53:22 +0600

    Add a nre row below with entry 1 as NEW and entry 2 as ROW

--- main.py
+++ main.py
@@ -9,4 +9,14 @@ table.add_column("Type", ["Legendary", "Electric", "Rock"]) #adds Type to headin
 
 print(table.align) #prints: {'base_align_value': 'c', 'Pokemon Name': 'c', 'Type': 'c'} which means align is set to c, center
 table.align = "l" #Aligns attributes to the left
+table.add_row(["NEW", "ROW"]) #adds a new row below with entry 1 as NEW and entry 2 as ROW
 print(table)
+#PRINTS:
+# +--------------+-----------+
+# | Pokemon Name | Type      |
+# +--------------+-----------+
+# | Celebi       | Legendary |
+# | Pikachu      | Electric  |
+# | Onix         | Rock      |
+# | NEW          | ROW       |
+# +--------------+-----------+
