7ae87b2a4b329a5771efe65ec9d9bbb69fb44bbf
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-28 19:50:06 +0600

    Align attributes to the left

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ table = PrettyTable() #this creates an empty table
 
 table.add_column("Pokemon Name", ["Celebi", "Pikachu", "Onix"]) #adds Pokemon Name to heading and "Celebi", "Pikachu", "Onix" as items in column
 table.add_column("Type", ["Legendary", "Electric", "Rock"]) #adds Type to heading and "Legendary", "Electric", "Rock" as items in column
+
+print(table.align) #prints: {'base_align_value': 'c', 'Pokemon Name': 'c', 'Type': 'c'} which means align is set to c, center
+table.align = "l" #Aligns attributes to the left
 print(table)
