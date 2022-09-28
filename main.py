f7f53c66923a29b5d5cea588b925b00e13fca14b
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-28 19:45:50 +0600

    Add Type to heading and "Legendary", "Electric", "Rock" as items in column

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ table = PrettyTable() #this creates an empty table
 
 
 table.add_column("Pokemon Name", ["Celebi", "Pikachu", "Onix"]) #adds Pokemon Name to heading and "Celebi", "Pikachu", "Onix" as items in column
-table.add_column("Type", "ggg")
+table.add_column("Type", ["Legendary", "Electric", "Rock"]) #adds Type to heading and "Legendary", "Electric", "Rock" as items in column
 print(table)
