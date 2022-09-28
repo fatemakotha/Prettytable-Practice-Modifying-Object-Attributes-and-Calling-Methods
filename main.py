30e192f77bad0f77ab7fa9e83d45b163d32139f5
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-28 19:44:22 +0600

    Add Pokemon Name to heading and "Celebi", "Pikachu", "Onix" as items in column

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import prettytable
 
 from prettytable import PrettyTable #from prettytable import the class Prettytablw
 table = PrettyTable() #this creates an empty table
+
+
+table.add_column("Pokemon Name", ["Celebi", "Pikachu", "Onix"]) #adds Pokemon Name to heading and "Celebi", "Pikachu", "Onix" as items in column
+table.add_column("Type", "ggg")
 print(table)
